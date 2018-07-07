763f7c0c2584ac5d4dd180c28977ec503d2edef8
s1250236 <s1250236@std6dc25.u-aizu.ac.jp>
2018-07-07 14:30:48 +0900

    Won/lost message added

--- dice1.c
+++ dice1.c
@@ -13,5 +13,7 @@ int main() {
   printf("Die 1:%d\n",ran1);
   printf("Die 2:%d\n",ran2);
   printf("Total value:%d\n",ran1+ran2);
+  if(ran1+ran2>7) printf("You won\n");
+  else printf("You lost\n");
   return 0;
 }
