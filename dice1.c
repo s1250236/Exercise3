82238e07cf793e4003f7a40c137180e624e35a48
s1250236 <s1250236@std6dc25.u-aizu.ac.jp>
2018-07-07 15:02:22 +0900

    Won/lost message modified

--- dice1.c
+++ dice1.c
@@ -4,6 +4,10 @@
 #include <unistd.h>
 int main() {
   int i,ran1,ran2;
+  char name[20];
+  printf("What is your name?\n>");
+  scanf("%s",name);
+  printf("Hello,%s!\n",name);
   printf("Rolling the dice...\n");
   srand(time(NULL));
   ran1 = (rand() % 6) + 1;
@@ -13,7 +17,7 @@ int main() {
   printf("Die 1:%d\n",ran1);
   printf("Die 2:%d\n",ran2);
   printf("Total value:%d\n",ran1+ran2);
-  if(ran1+ran2>7) printf("You won\n");
-  else printf("You lost\n");
+  if(ran1+ran2>7) printf("%s won\n",name);
+  else printf("%s lost\n",name);
   return 0;
 }
