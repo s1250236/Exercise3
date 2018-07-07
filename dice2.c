32bb41f3759625c38e4b6d2a17c47e7a841b51b3
s1250236 <s1250236@std6dc25.u-aizu.ac.jp>
2018-07-07 13:50:03 +0900

    Greetings added

---
+++ dice2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+int main() {
+  int i,ran1,ran2;
+  char name[20];
+  printf("What is your name?\n>");
+  scanf("%s",name);
+  printf("Hello,%s!\n",name);
+  printf("Rolling the dice...\n");
+  srand(time(NULL));
+  ran1 = (rand() % 6) + 1;
+  sleep(1);
+  srand(time(NULL));
+  ran2 = (rand() % 6) + 1;
+  printf("Die 1:%d\n",ran1);
+  printf("Die 2:%d\n",ran2);
+  printf("Total value:%d\n",ran1+ran2);
+  return 0;
+}
